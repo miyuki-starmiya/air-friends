d867563f58cd41cc8e0348f3e09e6cbfb8da184d
KamiHitoe <sousaku_0912@yahoo.co.jp>
2022-08-27 07:22:14 +0900

    done build

--- index.ts
+++ index.ts
@@ -1,29 +1,99 @@
-require('dotenv').config();
+// Import all dependencies, mostly using destructuring for better view.
+import { ClientConfig, Client, middleware, MiddlewareConfig, WebhookEvent, TextMessage, MessageAPIResponseBase } from '@line/bot-sdk';
+import { Application, Request, Response } from 'express';
+import 'dotenv/config';
 const express = require('express');
-const line = require('@line/bot-sdk');
 
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+// Setup all LINE client and Express configurations.
+const clientConfig: ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
-const app = express();
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
-})
+const middlewareConfig: MiddlewareConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET || '',
+};
 
-const client = new line.Client(config);
-const handleEvent = (event) => {
+const PORT: number | string = process.env.PORT || 3000;
+
+// Create a new LINE SDK client.
+const client = new Client(clientConfig);
+
+// Create a new Express application.
+const app: Application = express();
+
+// Function handler to receive the text.
+const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
+  // Process all variables here.
   if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
+    return;
   }
 
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text,
-  });
-}
+  // Process all message related variables here.
+  const { replyToken } = event;
+  const { text } = event.message;
 
-app.listen(3000);
+  // Create a new message.
+  const response: TextMessage = {
+    type: 'text',
+    text: `${text}って言った？`,
+  };
+
+  // Reply to the user.
+  await client.replyMessage(replyToken, response);
+};
+
+// Register the LINE middleware.
+// As an alternative, you could also pass the middleware in the route handler, which is what is used here.
+// app.use(middleware(middlewareConfig));
+
+// Route handler to receive webhook events.
+// This route is used to receive connection tests.
+app.get(
+  '/',
+  async (_: Request, res: Response): Promise<Response> => {
+    return res.status(200).json({
+      status: 'success',
+      message: 'Connected successfully!',
+    });
+  }
+);
+
+// This route is used for the Webhook.
+app.post(
+  '/webhook',
+  middleware(middlewareConfig),
+  async (req: Request, res: Response): Promise<Response> => {
+    const events: WebhookEvent[] = req.body.events;
+
+    // Process all of the received events asynchronously.
+    const results = await Promise.all(
+      events.map(async (event: WebhookEvent) => {
+        try {
+          await textEventHandler(event);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            console.error(err);
+          }
+
+          // Return an error message.
+          return res.status(500).json({
+            status: 'error',
+          });
+        }
+      })
+    );
+
+    // Return a successfull message.
+    return res.status(200).json({
+      status: 'success',
+      results,
+    });
+  }
+);
+
+// Create a server and listen to it.
+app.listen(PORT, () => {
+  console.log(`Application is live and listening on port ${PORT}`);
+});
