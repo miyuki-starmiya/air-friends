a3fba8d822930ade0781b065a787b048209c60ee
KamiHitoe <sousaku_0912@yahoo.co.jp>
2022-08-26 23:24:44 +0900

    yarn add, make index.ts

---
+++ index.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+var server = require("express");
+var ac = process.env.CHANNEL_SECRET;
+console.log(ac);
---
+++ index.ts
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const app = express();
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+})
+
+const client = new line.Client(config);
+const handleEvent = (event) => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text,
+  });
+}
+
+app.listen(3000);
